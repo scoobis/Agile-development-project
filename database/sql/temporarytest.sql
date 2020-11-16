ba40aeb6e5020ba27e96a7d812c66abfa3d15bcf
oh222fv <oh222fv@student.lnu.se>
2020-11-16 16:34:39 +0100

    Creating tables for Address and Postcode

--- database/sql/temporarytest.sql
+++ database/sql/temporarytest.sql
@@ -1,3 +1,4 @@
+-- USER --
 CREATE TABLE User (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(60) NOT NULL,
@@ -5,13 +6,39 @@ CREATE TABLE User (
   full_name VARCHAR(40) NOT NULL,
   role VARCHAR(20) NOT NULL,
   address_id INT(7),
-  PRIMARY KEY (id),
-  FOREIGN KEY (address_id) REFERENCES Address(id)
-)
+  PRIMARY KEY (id)
+);
 
 INSERT INTO User (email, password, full_name, role) 
 VALUES
-('admin@mail.com', 'admin', 'Admin Adminsson', 'ADMIN'),
-('producenten@mail.com', 'producenten', 'Produ Centen', 'PRODUCENT');
+  ('admin@mail.com', 'admin', 'Admin Adminsson', 'ADMIN'),
+  ('producenten@mail.com', 'producenten', 'Produ Centen', 'PRODUCENT');
 
+-- POSTCODE --
+CREATE TABLE Postcode (
+  code INT(5) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  PRIMARY KEY (code)
+);
 
+INSERT INTO Postcode (code, city) 
+VALUES
+  ('11111', 'Vaxsjo'),
+  ('11112', 'Vaxsjo');
+
+-- ADDRESS --
+CREATE TABLE Address (
+  id INT NOT NULL AUTO_INCREMENT,
+  street_name VARCHAR(50) NOT NULL,
+  street_no VARCHAR(15),
+  postcode INT(5) NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (postcode) REFERENCES Postcode(code),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+INSERT INTO Address (street_name, street_no, postcode, user_id)
+VALUES
+  ('Adminsgatan', 1, 11111, 1),
+  ('Producentbacken', 2, 11112, 2);
