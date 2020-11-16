ffeb9e9d28ca98008d5b5b636d82fe561fa4dd44
Adam Bergman <ab224qr@student.lnu.se>
2020-11-16 18:22:00 +0100

    Adding Home and Producer-Signup pages

---
+++ frontend/src/pages/sign-up/Producer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUpForm from '../../components/sign-up/producer/SignUpForm'
+
+function Producer () {
+  return (
+    <SignUpForm />
+  )
+}
+
+export default Producer
