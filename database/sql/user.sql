e93244a6e7e3a23912d6256a4e3700ea35c7a083
oh222fv <oh222fv@student.lnu.se>
2020-11-16 18:06:31 +0100

    Removing Address and Postcode tables, as they are not needed right now

--- database/sql/user.sql
+++ database/sql/user.sql
@@ -1,5 +1,5 @@
 -- USER --
-CREATE TABLE User (
+CREATE TABLE user (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(60) NOT NULL,
   password VARCHAR(100) NOT NULL,
@@ -8,36 +8,7 @@ CREATE TABLE User (
   PRIMARY KEY (id)
 );
 
-INSERT INTO User (email, password, full_name, role) 
+INSERT INTO user (email, password, full_name, role) 
 VALUES
   ('admin@mail.com', 'admin', 'Admin Adminsson', 'ADMIN'),
   ('producenten@mail.com', 'producenten', 'Produ Centen', 'PRODUCENT');
-
--- POSTCODE --
-CREATE TABLE Postcode (
-  code INT(5) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  PRIMARY KEY (code)
-);
-
-INSERT INTO Postcode (code, city) 
-VALUES
-  ('11111', 'Vaxsjo'),
-  ('11112', 'Vaxsjo');
-
--- ADDRESS --
-CREATE TABLE Address (
-  id INT NOT NULL AUTO_INCREMENT,
-  street_name VARCHAR(50) NOT NULL,
-  street_no VARCHAR(15),
-  postcode INT(5) NOT NULL,
-  user_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (postcode) REFERENCES Postcode(code),
-  FOREIGN KEY (user_id) REFERENCES User(id)
-);
-
-INSERT INTO Address (street_name, street_no, postcode, user_id)
-VALUES
-  ('Adminsgatan', 1, 11111, 1),
-  ('Producentbacken', 2, 11112, 2);
