8ba017b8908dd67c2b1dc50811bf82d7041334f5
oh222fv <oh222fv@student.lnu.se>
2020-11-16 16:15:17 +0100

    Creating a table for User

--- database/sql/temporarytest.sql
+++ database/sql/temporarytest.sql
@@ -1,5 +1,17 @@
-CREATE TABLE mytable (
-  data varchar(255)
-);
-INSERT INTO mytable (data)
-VALUES ("Kalle");
+CREATE TABLE User (
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(60) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  full_name VARCHAR(40) NOT NULL,
+  role VARCHAR(20) NOT NULL,
+  address_id INT(7),
+  PRIMARY KEY (id),
+  FOREIGN KEY (address_id) REFERENCES Address(id)
+)
+
+INSERT INTO User (email, password, full_name, role) 
+VALUES
+('admin@mail.com', 'admin', 'Admin Adminsson', 'ADMIN'),
+('producenten@mail.com', 'producenten', 'Produ Centen', 'PRODUCENT');
+
+
