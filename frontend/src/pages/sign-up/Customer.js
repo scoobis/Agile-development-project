1073715c9d38be4318d663f5b23c8dce986ff3a7
Adam Bergman <ab224qr@student.lnu.se>
2020-11-17 14:03:31 +0100

    Adding Sign Up page for customers

---
+++ frontend/src/pages/sign-up/Customer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUpForm from '../../components/sign-up/customer/SignUpForm'
+
+function Customer () {
+  return (
+    <SignUpForm />
+  )
+}
+
+export default Customer
