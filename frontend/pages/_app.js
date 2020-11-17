ee4363ac9d0fca0488a3e3806a308fa760469843
Adam Bergman <ab224qr@student.lnu.se>
2020-11-17 16:49:46 +0100

    Switching to Nextjs

---
+++ frontend/pages/_app.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../src/theme'
+import Header from '../src/components/layouts/Header'
+import Footer from '../src/components/layouts/Footer'
+
+export default function MyApp (props) {
+  const { Component, pageProps } = props
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Webbshop</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
+  )
+}
