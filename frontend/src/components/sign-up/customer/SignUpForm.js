c5fcfb5040ce2e89102a2558ec5905b346843ff0
Adam Bergman <ab224qr@student.lnu.se>
2020-11-17 14:03:10 +0100

    Adding customer registration component

---
+++ frontend/src/components/sign-up/customer/SignUpForm.js
@@ -0,0 +1,191 @@
+import React, { useState } from 'react'
+import { Button, TextField, FormControlLabel, Checkbox, Link, Container, Grid, Typography, FormControl, FormHelperText } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { isValidName, isValidEmail, isValidPassword } from '../../../helpers/user'
+import { isAlreadyRegistered, saveUser } from '../../../helpers/api'
+import { Customer } from '../../../helpers/roles'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
+
+export default function SignUpForm () {
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+  const [checked, setChecked] = useState(false)
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    password: false,
+    checked: false,
+    alreadyRegistered: false,
+    registrationFailed: false
+  })
+
+  const { name, email, password } = inputs
+
+  const handleChange = (e) => {
+    setAllErrorsTo(false)
+    setInputs({ ...inputs, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (isAlreadyRegistered(email)) {
+      setErrors({
+        ...errors,
+        alreadyRegistered: true
+      })
+      return
+    }
+
+    setErrors({
+      name: !isValidName(name),
+      email: !isValidEmail(email),
+      password: !isValidPassword(password),
+      checked: !checked
+    })
+
+    if (hasValidCredentials()) {
+      saveUser({ name, email, password, role: Customer })
+        .then(result => result.error
+          ? setErrors({ ...errors, registrationFailed: true })
+          : setErrors({ ...errors, registrationFailed: false }))
+    }
+  }
+
+  const setAllErrorsTo = (value) =>
+    setErrors(Object.assign(...Object.keys(errors).map(k => ({ [k]: value }))))
+
+  const hasValidCredentials = () =>
+    isValidName(name) &&
+    isValidEmail(email) &&
+    isValidPassword(password) &&
+    checked
+
+  const classes = useStyles()
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <div className={classes.paper}>
+        <Typography component='h1' variant='h5'>
+          Registrera kundkonto
+        </Typography>
+        <Typography>
+          När du registrerar ett konto får du ta del av fördelar som att kunna spara din adress, se orderhistorik etc.
+        </Typography>
+
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name='name'
+                label='Fullständigt namn'
+                variant='outlined'
+                fullWidth
+                autoFocus
+                required
+                value={name}
+                onChange={handleChange}
+                error={errors.name}
+                helperText={errors.name && 'Ogiltigt namn'}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name='email'
+                label='E-post'
+                variant='outlined'
+                required
+                value={email}
+                onChange={handleChange}
+                error={errors.email}
+                helperText={errors.email && 'Ogiltig e-post'}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name='password'
+                label='Lösenord'
+                variant='outlined'
+                type='password'
+                required
+                fullWidth
+                value={password}
+                onChange={handleChange}
+                error={errors.password}
+                helperText={errors.password && 'Lösenordet är för svagt'}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl required error={!checked}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color='primary'
+                      checked={checked}
+                      onChange={(e) => {
+                        setChecked(e.target.checked)
+                        setErrors({ ...errors, checked: !e.target.checked })
+                      }}
+                    />
+                  }
+                  label='Jag har läst och godkänner köpvillkoren.'
+                />
+                {errors.checked && (
+                  <FormHelperText>Villkoren måste godkännas.</FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              {errors.alreadyRegistered || errors.registrationFailed ? (
+                <Typography component='p' color='error'>
+                  {errors.alreadyRegistered
+                    ? 'E-postadressen är redan registrerad.'
+                    : 'Registrering misslyckades.'}
+                </Typography>
+              ) : ''}
+            </Grid>
+          </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Registrera
+          </Button>
+          <Grid container justify='center'>
+            <Grid item>
+              <Link href='#' variant='body2'>
+                Är du redan kund? Logga in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  )
+}
