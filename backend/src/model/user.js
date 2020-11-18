ff0dd4f1573c7600202457b4fbfb9dcc667805e8
seisser <jw22gh@student.lnu.se>
2020-11-18 16:41:00 +0100

    Creating a class representing a user

---
+++ backend/src/model/user.js
@@ -0,0 +1,66 @@
+
+class user {
+    constructor(email, password, name, role) {
+        this.email = email
+        this.password = password
+        this.name = name
+        this.role = role
+    }
+
+    /**
+     * @param {any} id
+     */
+    set id(id) {
+        this._id = id
+    }
+
+    get id() {
+        return this._id
+    }
+
+    /**
+     * @param {string} email
+     */
+    set email(email) {
+        this._email = email
+    }
+
+    get email() {
+        return this._email
+    }
+
+    /**
+     * @param {string} password
+     */
+    set password(password) {
+        this._password = password
+    }
+
+    get password() {
+        return this._password
+    }
+
+    /**
+     * @param {string} name
+     */
+    set name(name) {
+        this._name = name
+    }
+
+    get name() {
+        return this._name
+    }
+
+    /**
+     * @param {roles} role
+     */
+    set role(role) {
+        this._role = role
+    }
+
+    get role() {
+        return this._role
+    }
+}
+
+module.exports = user
