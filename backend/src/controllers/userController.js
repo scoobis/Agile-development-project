0ee37444bcead60e0700e486b2e4dafeab498197
seisser <jw22gh@student.lnu.se>
2020-11-23 00:56:09 +0100

    Moving code to controller from service so the service can be reused

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const service = require('../services/userService')
+const createError = require('http-errors')
 
 const controller = {}
 
@@ -9,7 +10,39 @@ controller.test = async (req, res, next) => {
 }
 
 controller.create = async (req, res, next) => {
-  service.create(req, res, next)
+  const { name, email, password } = req.body
+
+
+  if (!name && !email && !password) {
+    const error = createError(400, 'No parameters entered!')
+    return next(error)
+  }
+
+  const isValidPassword = service.isValidPassword(password)
+  const isValidName = service.isValidName(name)
+  const isValidEmail = service.isValidEmail(email)
+  const isAlreadyRegistered = service.isAlreadyRegistered(email)
+
+  if (!isValidPassword) {
+    const error = createError(400, 'Password needs to be at least 6 characters long!')
+    return next(error)
+  } else if (!isValidName) {
+    const error = createError(400, 'Name needs to be at least 3 characters long!')
+    return next(error)
+  } else if (!isValidEmail) {
+    const error = createError(400, 'Not a valid email address!')
+    return next(error)
+  } else if (isAlreadyRegistered) {
+    const error = createError(400, 'Email already exists')
+    return next(error)
+  } else {
+    try {
+      service.create(username, email, password)
+      res.send({ success: true, message: 'Successfully created user' })
+    } catch (error) {
+      return next(error)
+    }
+  }
 }
 
 module.exports = controller
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,31 +1,15 @@
 const user = require('../models/user')
 const userDAO = require('../database/userDAO')
-const createError = require('http-errors')
 
 const service = {}
 
 service.create = async (req, res, next) => {
-  
-    if (!isValidEmail(req.body.email)) {
-      const error = createError(400, 'Not a valid email address!')
-      return next(error)
-    } else if (!isValidName(req.body.name)) {
-      const error = createError(400, 'Name needs to be at least 3 characters long!')
-      return next(error)
-    } else if (!isValidPassword(req.body.password)) {
-      const error = createError(400, 'Password needs to be at least 6 characters long!')
-      return next(error)
-    } else if (await isAlreadyRegistered(req.body.email)) {
-      const error = createError(400, 'Email already exists')
-      return next(error)
-    } else {
+
       const userToRegister = new user(
         req.body.email, req.body.password, req.body.name, req.body.role
       )
       await userDAO.create(userToRegister)
 
-      res.status(200).json({ 'success': true, 'message' : 'Account created!'})
-    } 
    
 }
 
@@ -36,7 +20,7 @@ service.create = async (req, res, next) => {
  * @param {*} name 
  * @returns {boolean} - True if valid / False if not valid.
  */
-const isValidName = (name) => name.length >= 3
+service.isValidName = (name) => name.length >= 3
 
 /**
  * Checks if password is long enough
@@ -44,7 +28,7 @@ const isValidName = (name) => name.length >= 3
  * @param {*} pwd
  * @returns {boolean} - True if valid / False if not valid.
  */
-const isValidPassword = (pwd) => pwd.length >= 6
+service.isValidPassword = (pwd) => pwd.length >= 6
 
 /**
  * Uses a regex to check if the email is valid.
@@ -52,7 +36,7 @@ const isValidPassword = (pwd) => pwd.length >= 6
  * @param {string} email - Email address.
  * @returns {boolean} - True if valid / False if not valid.
  */
-const isValidEmail = (email) =>
+service.isValidEmail = (email) =>
   /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)
 
 /**
@@ -61,7 +45,7 @@ const isValidEmail = (email) =>
  * @param {string} email - Email address.
  * @returns {boolean} - True if already in use/False if not
  */
-const isAlreadyRegistered = async (email) => {
+service.isAlreadyRegistered = async (email) => {
   let user = await userDAO.findByEmail(email)
   if (typeof user == 'object') {
     return true
