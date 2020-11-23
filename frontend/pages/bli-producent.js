65e9c75264720420534b9aa16bc5048ee4bd06d6
scoobis <gabriel.bjorlin@gmail.com>
2020-11-23 10:53:33 +0100

    initial setup

--- frontend/pages/bli-producent.js
+++ frontend/pages/bli-producent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProducerSignupForm from '../src/components/signup/ProducerSignupForm'
 
-function BecomeProducer () {
+function BecomeProducer() {
   return (
     <ProducerSignupForm />
   )
---
+++ frontend/src/components/products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+const Products = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <h1>Test</h1>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Products
