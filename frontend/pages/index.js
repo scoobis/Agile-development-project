2eb17f94a8c0ac9f48b7e9c5512f3d6e368cd39a
scoobis <gabriel.bjorlin@gmail.com>
2020-11-24 18:51:01 +0100

    adding path to products from index page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,17 +1,27 @@
 import { Container, Typography } from '@material-ui/core'
 import Link from 'next/link'
 
-export default function Home () {
+export default function Home() {
   return (
     <Container maxWidth='sm'>
+      <Typography>Hello from Home!</Typography>
       <Typography>
-        Hello from Home!
+        Registrera dig som producent{' '}
+        <Link href='/bli-producent'>
+          <a>här</a>
+        </Link>
       </Typography>
       <Typography>
-        Registrera dig som producent <Link href='/bli-producent'><a>här</a></Link>
+        Registrera dig som kund{' '}
+        <Link href='/registrera'>
+          <a>här</a>
+        </Link>
       </Typography>
       <Typography>
-        Registrera dig som kund <Link href='/registrera'><a>här</a></Link>
+        See all produkter{' '}
+        <Link href='/produkter'>
+          <a>här</a>
+        </Link>
       </Typography>
     </Container>
   )
