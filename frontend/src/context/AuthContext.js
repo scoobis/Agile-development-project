014a13e243fa6f574f02f86d48fe2ff14bb581da
Adam Bergman <ab224qr@student.lnu.se>
2020-11-25 13:04:45 +0100

    Adding signin and signout and user-object

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,22 +1,63 @@
 import React, { useState, createContext, useEffect } from 'react'
-import { getTokenCookie } from '../utils/auth-cookies'
+import { setUserCookie, getUserCookie, removeUserCookie } from '../utils/auth-cookies'
+import { login } from '../utils/api'
+import { Producer, Customer } from '../utils/roles'
+import Router from 'next/router'
 
 export const AuthContext = createContext()
 
 const AuthProvider = props => {
-  const [isAuthenticated, setAuthenticated] = useState(false)
+  const [user, setUser] = useState({
+    isAuthenticated: false,
+    user: {}
+  })
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      const hasToken = getTokenCookie()
-      if (hasToken) {
-        setAuthenticated(true)
+    if (!user.isAuthenticated) {
+      const userCookie = getUserCookie()
+      if (userCookie) {
+        setUser({ ...user, isAuthenticated: true, user: userCookie })
       }
     }
   }, [])
 
+  const authenticate = (user) => {
+    return login(user).then(response => {
+      if (response.status === 200) {
+        const userToSave = {
+          name: response.data.name,
+          email: response.data.email,
+          role: response.data.role,
+          token: response.data.token
+        }
+
+        setUser({ ...user, isAuthenticated: true, user: userToSave })
+        setUserCookie(userToSave)
+      }
+
+      return response
+    })
+  }
+
+  const signout = () => {
+    Router.push('/')
+    setUser({ ...user, isAuthenticated: false, user: {} })
+    removeUserCookie()
+  }
+
+  const isProducer = user.isAuthenticated && user.user.role === Producer
+  const isCustomer = user.isAuthenticated && user.user.role === Customer
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setAuthenticated }}>
+    <AuthContext.Provider value={{
+      authenticate,
+      signout,
+      user,
+      setUser,
+      isProducer,
+      isCustomer
+    }}
+    >
       {props.children}
     </AuthContext.Provider>
   )
