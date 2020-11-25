807bff150ce5b95d732cca66ed64a67b20f7e1cf
oh222fv <oh222fv@student.lnu.se>
2020-11-25 10:41:32 +0100

    Breaking out validation as middleware

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -6,70 +6,17 @@ const controller = {}
 
 controller.test = async (req, res, next) => {
   try {
-    //startsidan
+    res.status(200).json({message: 'blslsls'})
   } catch (err) { next(err) }
 }
 
 controller.create = async (req, res, next) => {
-  // User
-  const { name, email, password, role } = req.body
-
-  if (!name && !email && !password) {
-    const error = createError(400, 'No parameters entered!')
-    return next(error)
-  }
-
-  const isValidPassword = service.isValidPassword(password)
-  const isValidName = service.isValidName(name)
-  const isValidEmail = service.isValidEmail(email)
-  const isAlreadyRegistered = service.isAlreadyRegistered(email)
-
-  if (!isValidPassword) {
-    const error = createError(400, 'Password needs to be at least 6 characters long!')
-    return next(error)
-  } else if (!isValidName) {
-    const error = createError(400, 'Name needs to be at least 3 characters long!')
-    return next(error)
-  } else if (!isValidEmail) {
-    const error = createError(400, 'Not a valid email address!')
-    return next(error)
-  } else if (await isAlreadyRegistered) {
-    const error = createError(400, 'Email already exists')
-    return next(error)
-  } 
-  
-  // Producer
-  if (role === 'producer') { //Create function/enum?
-
-    const { orgNumber, phone, zip } = req.body
-
-    const isValidOrganizationNumber = service.isValidOrganizationNumber(orgNumber)
-    const isValidPhoneNumber = service.isValidPhoneNumber(phone)
-    const isValidZipCode = service.isValidZipCode(zip)
-    const isOrgNumberAlreadyInUse = service.isOrgNumberAlreadyInUse(orgNumber)
-
-    if (!isValidOrganizationNumber) {
-      const error = createError(400, 'Not a valid organization number')
-      return next(error)
-    } else if (!isValidPhoneNumber){
-      const error = createError(400, 'Not a valid phone number')
-      return next(error)
-    } else if (!isValidZipCode) {
-      const error = createError(400, 'Not a valid zip code')
-      return next(error)
-    } else if (await isOrgNumberAlreadyInUse) {
-      const error = createError(400, 'Organization number already exists')
-      return next(error)
-    }
-  }
-
   try {
     service.create(req)
     res.status(200).json({ 'success': true, 'message' : 'Account created!'})
   } catch (error) {
     return next(error)
-  }
-  
+  } 
 }
 
 controller.login = async (req, res, next) => {
