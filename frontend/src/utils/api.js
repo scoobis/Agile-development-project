b38c8940051b4d671442107cd5eae486d27f1c10
Adam Bergman <ab224qr@student.lnu.se>
2020-11-25 13:04:04 +0100

    Setting authentication token header when successful login

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,15 @@
-import axios from './axios-wrapper'
+import axios, { setAuthToken } from './axios-wrapper'
 
 export const signup = (data) => axios.post('/signup', data)
   .then(response => response)
   .catch(err => err.response)
 
 export const login = (user) => axios.post('/login', user)
-  .then(response => response)
+  .then(response => {
+    if (response.status === 200) {
+      setAuthToken(response.data.token)
+    }
+
+    return response
+  })
   .catch(err => err.response)
