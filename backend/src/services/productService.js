ba1708d19caa262c349a8c00cad35449e8af07f4
Adam Bergman <ab224qr@student.lnu.se>
2020-11-30 15:19:46 +0100

    Adding orgNumber and changing desc to description and removing await in return statement

--- backend/src/services/productService.js
+++ backend/src/services/productService.js
@@ -1,25 +1,25 @@
-const product = require('../models/product')
+const Product = require('../models/product')
 const productDAO = require('../database/productDAO')
 
 const service = {}
 
 /**
  * Creates a new product
- * 
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  */
-service.create = async (req, res, next) => {  
-  const newProduct = new product(
-    1234567897,  //(req.body.orgNumber)  Tillfälligt hårdkodad för att matcha testproducent i databasen
+service.create = async (req, res, next) => {
+  const newProduct = new Product(
+    req.body.orgNumber,
     req.body.name,
-    req.body.desc,
+    req.body.description,
     req.body.price,
     req.body.unit,
     req.body.inStock
   )
-  
+
   const categoryId = 1 // (req.body.categoryId) Tillfälligt hårdkodad för att matcha testkategori i databasen // One or more? Add to product model?
 
   await productDAO.create(newProduct, categoryId)
@@ -27,15 +27,16 @@ service.create = async (req, res, next) => {
 
 service.get = async (req, res, next) => {
   const productId = await req.params.id
-  return await productDAO.get(productId)
+  const product = await productDAO.get(productId)
+  return product
 }
 
-service.getAll = async (req, res, next) => {  
+service.getAll = async (req, res, next) => {
   // Todo: Format them specifically?
   return productDAO.getAll()
 }
 
-service.getAllFromProducer = async (req, res, next) => {  
+service.getAllFromProducer = async (req, res, next) => {
   const orgNumber = await req.params.org_no
   // Todo: Format them specifically?
   return productDAO.getAllByOrgNumber(orgNumber)
