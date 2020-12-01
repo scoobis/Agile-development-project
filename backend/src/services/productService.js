b3c1da4f86fcf7364f945e2ade423cd872ac0f4d
oh222fv <oh222fv@student.lnu.se>
2020-12-01 08:54:24 +0100

    Creating productService.update() and breaking out product creating

--- backend/src/services/productService.js
+++ backend/src/services/productService.js
@@ -11,7 +11,73 @@ const service = {}
  * @param {*} next
  */
 service.create = async (req, res, next) => {
-  const newProduct = new Product(
+  const newProduct = await service.getProductFromRequest(req)
+
+  const categoryId = 1 // (req.body.categoryId) Tillfälligt hårdkodad för att matcha testkategori i databasen // One or more? Add to product model?
+
+  await productDAO.create(newProduct, categoryId)
+}
+
+/**
+ * Updates a product
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+service.update = async (req, res, next) => {
+  const updatedProduct = await service.getProductFromRequest(req)
+
+  const categoryId = 1 // (req.body.categoryId) Tillfälligt hårdkodad för att matcha testkategori i databasen // One or more? Add to product model?
+
+  await productDAO.create(updatedProduct, categoryId)
+}
+
+/**
+ * Gets one product
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+service.get = async (req, res, next) => {
+  const productId = await req.params.id
+  const product = await productDAO.get(productId)
+  return product
+}
+
+/**
+ * Gets all products
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+service.getAll = async (req, res, next) => {
+  // Todo: Format them specifically?
+  return productDAO.getAll()
+}
+
+/**
+ * Gets all products from one producer
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+service.getAllFromProducer = async (req, res, next) => {
+  const orgNumber = await req.params.org_no
+  // Todo: Format them specifically?
+  return productDAO.getAllByOrgNumber(orgNumber)
+}
+
+/**
+ * Creates and returns a Product object out of the request data
+ *
+ * @param {*} req
+ */
+service.getProductFromRequest = async (req) => {
+  return new Product(
     req.body.orgNumber,
     req.body.name,
     req.body.description,
@@ -19,27 +85,6 @@ service.create = async (req, res, next) => {
     req.body.unit,
     req.body.inStock
   )
-
-  const categoryId = 1 // (req.body.categoryId) Tillfälligt hårdkodad för att matcha testkategori i databasen // One or more? Add to product model?
-
-  await productDAO.create(newProduct, categoryId)
-}
-
-service.get = async (req, res, next) => {
-  const productId = await req.params.id
-  const product = await productDAO.get(productId)
-  return product
-}
-
-service.getAll = async (req, res, next) => {
-  // Todo: Format them specifically?
-  return productDAO.getAll()
-}
-
-service.getAllFromProducer = async (req, res, next) => {
-  const orgNumber = await req.params.org_no
-  // Todo: Format them specifically?
-  return productDAO.getAllByOrgNumber(orgNumber)
 }
 
 module.exports = service
