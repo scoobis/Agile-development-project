44c90ff0c4040bac6ebae76b282802e8eb02c4d2
Gabriel Björlin <gb222hq@student.lnu.se>
2020-12-01 15:39:35 +0100

    initial design

--- frontend/src/components/products/Products.js
+++ frontend/src/components/products/Products.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import { Grid } from '@material-ui/core'
 import ProductCard from './ProductCard'
 import { ProductContext } from '../../context/ProductContext'
+import FilterMenu from './FiliterMenu'
 
 const Products = () => {
   const { products } = useContext(ProductContext)
 
   return (
     <Grid container spacing={2}>
+      <FilterMenu />
       {products.map((product) => {
         return (
           <Grid item xs={12} sm={6} lg={3} key={product.id}>
