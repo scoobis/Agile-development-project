0f04980c06a5f7db5d564bf096e98de6756fda9d
oh222fv <oh222fv@student.lnu.se>
2020-12-02 19:48:55 +0100

    Adding error handling for updating product

--- backend/src/database/productDAO.js
+++ backend/src/database/productDAO.js
@@ -23,9 +23,9 @@ productDAO.create = async (product, categoryId) => {
     )
     const productId = productResponse.insertId
 
-    await conn.query(`INSERT INTO product_category (product_id, category_id) VALUES ('${productId}', '${categoryId}')`)
-  } catch (error) {
-    throw error
+    await conn.query(
+      'INSERT INTO product_category value (?, ?)', [productId, categoryId]
+    )
   } finally {
     if (conn) conn.release()
   }
@@ -44,14 +44,20 @@ productDAO.update = async (product, categoryId) => {
 
     const { id, orgNumber, name, desc, price, unit, inStock } = product
     // TODO: Fix Transaction.
-    await conn.query(
+
+    const result = await conn.query(
       'UPDATE product SET producer_org_no=?, name=?, description=?, price=?, unit=?, in_stock=? WHERE id=?',
       [orgNumber, name, desc, price, unit, inStock, id]
     )
 
-    await conn.query(
-      'UPDATE product_category SET category_id=? WHERE product_id=?', [categoryId, id]
-    )
+    if (result.affectedRows) {
+      // If exists, else INSERT
+      await conn.query(
+        'UPDATE product_category SET category_id=? WHERE product_id=?', [categoryId, id]
+      )
+    } else {
+      throw createError(400, 'Product not found!')
+    }
   } finally {
     if (conn) conn.release()
   }
@@ -87,8 +93,6 @@ productDAO.get = async (productId) => {
     conn = await pool.getConnection()
     const [row] = await conn.query(`SELECT * FROM product WHERE id=('${productId}')`)
     return row
-  } catch (error) {
-    throw error
   } finally {
     if (conn) conn.release()
   }
@@ -105,8 +109,6 @@ productDAO.getAll = async () => {
     // Todo: Limit this for X amount of rows.
     const rows = await conn.query('SELECT * FROM product')
     return rows
-  } catch (error) {
-    throw error
   } finally {
     if (conn) conn.release()
   }
@@ -124,8 +126,6 @@ productDAO.getAllByOrgNumber = async (orgNumber) => {
     // Todo: Limit this for X amount of rows.
     const rows = await conn.query(`SELECT * FROM product WHERE producer_org_no=('${orgNumber}')`)
     return rows
-  } catch (error) {
-    throw error
   } finally {
     if (conn) conn.release()
   }
@@ -137,8 +137,6 @@ productDAO.getAllCategories = async () => {
     conn = await pool.getConnection()
     const categories = await conn.query('SELECT name, id FROM category WHERE parent_id IS NULL')
     return categories
-  } catch (error) {
-    throw error
   } finally {
     if (conn) conn.release()
   }
@@ -150,8 +148,6 @@ productDAO.getAllSubCategories = async () => {
     conn = await pool.getConnection()
     const subcategories = await conn.query('SELECT name, id, parent_id FROM category WHERE parent_id IS NOT NULL')
     return subcategories
-  } catch (error) {
-    throw error
   } finally {
     if (conn) conn.release()
   }
