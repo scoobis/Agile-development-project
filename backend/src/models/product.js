2bf1096b04e2fe5ec34726766a09e38e8c018507
Kevin Cederholm <kc222en@student.lnu.se>
2020-12-07 12:57:35 +0100

    Updating product model

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -1,12 +1,15 @@
 class product {
-  constructor (orgNumber, name, desc, price, unit, inStock, categories) {
+  constructor (id = null, orgNumber, name, desc, price, salePrice = null, unit, inStock, categories = [], images = []) {
+    this.id = id
     this.orgNumber = orgNumber
     this.name = name
     this.desc = desc
     this.price = price
+    this.salePrice = salePrice
     this.unit = unit
     this.inStock = inStock
     this.categories = categories
+    this.images = images
   }
 }
 
