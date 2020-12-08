1e8bae964e6956517c6058b7270d27f999a02689
oh222fv <oh222fv@student.lnu.se>
2020-12-08 19:15:32 +0100

    Adding validate.product to update product route

--- backend/src/routes/productRouter.js
+++ backend/src/routes/productRouter.js
@@ -6,7 +6,7 @@ const imageUpload = require('../middlewares/imageupload')
 
 router.post('/', validate.product, imageUpload.product, controller.create)
 router.get('/:id', authorize.ownerOfProduct, controller.get)
-router.put('/:id', authorize.ownerOfProduct, controller.update)
+router.put('/:id', validate.procuct, authorize.ownerOfProduct, controller.update)
 router.delete('/:id', authorize.ownerOfProduct, controller.delete)
 
 module.exports = router
