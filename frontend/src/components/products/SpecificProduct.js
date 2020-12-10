6fa531b3736064cce8970bb386f8f4a25b17c41b
Adam Bergman <ab224qr@student.lnu.se>
2020-12-10 09:20:23 +0100

    Displaying product image from backend on specific product

--- frontend/src/components/products/SpecificProduct.js
+++ frontend/src/components/products/SpecificProduct.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Container, Typography, Grid } from '@material-ui/core'
 import SpecificProductCard from './SpecificProductCard'
 import { getOneProduct } from '../../utils/api'
+import { API_URL } from '../../utils/config'
 
 const useStyles = makeStyles({
   img: {
@@ -20,19 +21,24 @@ const SpecificProduct = (props) => {
 
   useEffect(() => {
     getOneProduct(productId).then((response) => {
+      console.log(response)
       setProduct(response)
       onLoad(response.name)
     })
   }, [props])
 
-  const { name, description, price, unit, inStock, id } = product
-  const imgSrc = 'apples.JPG' // TODO: Get img from backend
+  const { name, description, price, unit, inStock, id, images } = product
+  const DEFAULT_IMG_SRC = '/apples.JPG'
 
   return (
     <Container className={classes.container}>
       <Grid className={classes.gridContainer} container spacing={2}>
         <Grid item xs={7}>
-          <img className={classes.img} alt='Produkt bild' src={`/${imgSrc}`} />
+          {images && images.length ? (
+            images.map((img) => <img alt={img.alt_text} key={img.id} src={`${API_URL}/static/${img.image_name}`} />)
+          ) : (
+            <img className={classes.img} alt='Produkt bild' src={DEFAULT_IMG_SRC} />
+          )}
         </Grid>
         <Grid item xs={5}>
           <SpecificProductCard inStock={inStock} price={price} name={name} unit={unit} id={id} />
