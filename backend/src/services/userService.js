76ede2cd64f49d335fa56fc416daf89c4ba9e009
oh222fv <oh222fv@student.lnu.se>
2020-12-11 17:13:11 +0100

    Breaking out validation from userService to validateUserService

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -36,6 +36,7 @@ service.createProducer = async (req, res, next) => {
  * @param {object} req
  * @param {object} res
  * @param {object} next
+ * @returns {object} user //TODO Return Producer/User instead?
  */
 service.login = async (req, res, next) => {
   const user = await userDAO.login(req)
@@ -50,6 +51,22 @@ service.login = async (req, res, next) => {
     }
   }
   return user
+}
+
+/**
+ * Returns an orgNumber from an email
+ *
+ * @param {String} email
+ */
+service.getOrgNumberByEmail = async (email) => {
+  const user = await userDAO.findByEmail(email)
+  if (typeof user === 'object') {
+    const id = user.id
+    const userToReturn = await userDAO.getProducerByUserId(id)
+    return userToReturn.org_no
+  } else {
+    return false
+  }
 }
 
 /**
@@ -100,106 +117,6 @@ service.parseAddressFromRequest = async (req) => {
     req.body.city,
     'business'
   )
-}
-
-/**
- * Checks if name is long enough
- *
- * @param {*} name
- * @returns {boolean} - True if valid / False if not valid.
- */
-service.isValidName = (name) => name.length >= 3
-
-/**
- * Checks if password is long enough
- *
- * @param {*} pwd
- * @returns {boolean} - True if valid / False if not valid.
- */
-service.isValidPassword = (pwd) => pwd.length >= 6
-
-/**
- * Uses a regex to check if the email is valid.
- *
- * @param {string} email - Email address.
- * @returns {boolean} - True if valid / False if not valid.
- */
-service.isValidEmail = (email) =>
-  /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)
-
-/**
- * Checks if the provided email address is already in use.
- *
- * @param {string} email - Email address.
- * @returns {boolean} - True if already in use/False if not
- */
-service.isAlreadyRegistered = async (email) => {
-  const user = await userDAO.findByEmail(email)
-  if (typeof user === 'object') {
-    return true
-  } else {
-    return false
-  }
-}
-
-/**
- * Checks if organization number is valid
- *
- * @param {*} num
- */
-service.isValidOrganizationNumber = (num) => {
-  let n = num
-
-  if (typeof num === 'string') {
-    n = num.replace(/\D/g, '')
-  }
-
-  return n.length === 10
-}
-
-/**
- * Checks if zip code is valid
- *
- * @param {*} code
- */
-service.isValidZipCode = (code) => code.length === 5
-
-/**
- * Checks if phone number is valid
- *
- * @param {*} number
- */
-service.isValidPhoneNumber = number => number.length === 10
-
-/**
- * Checks if the provided organisation number is already in use.
- *
- * @param {*} orgNumber - Email address.
- * @returns {boolean} - True if already in use/False if not
- */
-service.isOrgNumberAlreadyInUse = async (orgNumber) => {
-  const producer = await userDAO.getProducerByOrgNumber(orgNumber)
-  if (typeof producer === 'object') {
-    return true
-  } else {
-    return false
-  }
-}
-
-/**
- * Returns an orgNumber from an email
- *
- * @param {*} email
- */
-service.getOrgNumberByEmail = async (email) => {
-  const user = await userDAO.findByEmail(email)
-  if (typeof user === 'object') {
-    const id = user.id
-    const userToReturn = await userDAO.getProducerByUserId(id)
-    return userToReturn.org_no
-  } else {
-    return false
-  }
 }
 
 module.exports = service
---
+++ backend/src/services/validateUserService.js
@@ -0,0 +1,89 @@
+const userDAO = require('../database/userDAO')
+
+const service = {}
+
+/**
+ * Checks if name is long enough
+ *
+ * @param {*} name
+ * @returns {boolean} - True if valid / False if not valid.
+ */
+service.isValidName = (name) => name.length >= 3
+
+/**
+ * Checks if password is long enough
+ *
+ * @param {*} pwd
+ * @returns {boolean} - True if valid / False if not valid.
+ */
+service.isValidPassword = (pwd) => pwd.length >= 6
+
+/**
+ * Uses a regex to check if the email is valid.
+ *
+ * @param {string} email - Email address.
+ * @returns {boolean} - True if valid / False if not valid.
+ */
+service.isValidEmail = (email) =>
+  /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+/**
+ * Checks if the provided email address is already in use.
+ *
+ * @param {string} email - Email address.
+ * @returns {boolean} - True if already in use/False if not
+ */
+service.isAlreadyRegistered = async (email) => {
+  const user = await userDAO.findByEmail(email)
+  if (typeof user === 'object') {
+    return true
+  } else {
+    return false
+  }
+}
+
+/**
+ * Checks if organization number is valid
+ *
+ * @param {*} num
+ */
+service.isValidOrganizationNumber = (num) => {
+  let n = num
+
+  if (typeof num === 'string') {
+    n = num.replace(/\D/g, '')
+  }
+
+  return n.length === 10
+}
+
+/**
+ * Checks if zip code is valid
+ *
+ * @param {*} code
+ */
+service.isValidZipCode = (code) => code.length === 5
+
+/**
+ * Checks if phone number is valid
+ *
+ * @param {*} number
+ */
+service.isValidPhoneNumber = number => number.length === 10
+
+/**
+ * Checks if the provided organisation number is already in use.
+ *
+ * @param {*} orgNumber - Email address.
+ * @returns {boolean} - True if already in use/False if not
+ */
+service.isOrgNumberAlreadyInUse = async (orgNumber) => {
+  const producer = await userDAO.getProducerByOrgNumber(orgNumber)
+  if (typeof producer === 'object') {
+    return true
+  } else {
+    return false
+  }
+}
+
+module.exports = service
