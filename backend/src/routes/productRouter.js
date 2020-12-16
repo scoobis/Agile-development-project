0db9d62a7b1f4ebbe3da065743768f0842ab17f3
oh222fv <oh222fv@student.lnu.se>
2020-12-16 20:34:19 +0100

    Changing order in router

--- backend/src/routes/productRouter.js
+++ backend/src/routes/productRouter.js
@@ -7,16 +7,16 @@ const fileUpload = require('../middlewares/fileupload')
 // Categories
 router.get('/categories', controller.getAllCategories)
 
-// Product
-router.post('/', authorize.verifyJWT, fileUpload.product, validate.product, controller.create, fileUpload.errorHandler)
-router.get('/:id', controller.get)
-router.put('/:id', authorize.verifyJWT, fileUpload.product, validate.product, controller.update, fileUpload.errorHandler)
-router.delete('/:id', authorize.verifyJWT, controller.delete)
-
 // Products
 router.get('/all', controller.getAll)
 router.get('/category/:categoryId', controller.getAllFromCategory)
 router.get('/producer/:orgNumber', controller.getAllFromProducer)
 router.get('/all/search?', controller.getAllByName)
+
+// Product
+router.post('/', authorize.verifyJWT, fileUpload.product, validate.product, controller.create, fileUpload.errorHandler)
+router.get('/:id', controller.get)
+router.put('/:id', authorize.verifyJWT, fileUpload.product, validate.product, controller.update, fileUpload.errorHandler)
+router.delete('/:id', authorize.verifyJWT, controller.delete)
 
 module.exports = router
