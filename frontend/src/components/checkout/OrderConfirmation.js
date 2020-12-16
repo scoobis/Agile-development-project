0b62093159c5aa37b51abe22f18c63be7fc26659
Gabriel Björlin <gb222hq@student.lnu.se>
2020-12-16 14:42:54 +0100

    initial information

--- frontend/src/components/checkout/OrderConfirmation.js
+++ frontend/src/components/checkout/OrderConfirmation.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Container, Typography, Box } from '@material-ui/core'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 
 const OrderConfirmation = () => {
-  return <h1>Order</h1>
+  return (
+    <Container align='center'>
+      <Typography variant='h2'>Tack För Din Order</Typography>
+      <CheckCircleIcon style={{ fontSize: 55, marginTop: '20px', color: 'green' }} />
+      <Box></Box>
+    </Container>
+  )
 }
 
 export default OrderConfirmation
