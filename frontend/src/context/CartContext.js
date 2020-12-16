65943e24ea520034057ef6d07954d9da1baf18a6
Gabriel Björlin <gb222hq@student.lnu.se>
2020-12-16 19:33:56 +0100

    clear cart context

--- frontend/src/context/CartContext.js
+++ frontend/src/context/CartContext.js
@@ -18,12 +18,9 @@ const CartContextProvider = (props) => {
   const increase = (payload) => dispatch({ type: 'INCREASE', payload })
   const decrease = (payload) => dispatch({ type: 'DECREASE', payload })
   const removeProduct = (payload) => dispatch({ type: 'REMOVE_PRODUCT', payload })
+  const clearCart = () => dispatch({ type: 'CLEAR_CART' })
 
-  return (
-    <CartContext.Provider value={{ state, addProduct, increase, decrease, removeProduct }}>
-      {props.children}
-    </CartContext.Provider>
-  )
+  return <CartContext.Provider value={{ state, addProduct, increase, decrease, removeProduct, clearCart }}>{props.children}</CartContext.Provider>
 }
 
 export default CartContextProvider
